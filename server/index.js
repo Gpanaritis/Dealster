b82c95a2b03131949f31e40d3c65ec0443bebe31
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2022-12-25 20:15:07 +0200

    Updated index.js. Fixed products

--- server/index.js
+++ server/index.js
@@ -9,7 +9,21 @@ const db = require('./models');
 
 // Routers
 const usersRouter = require('./routes/Users');
+const categoriesRouter = require('./routes/Category');
+const subcategoriesRouter = require('./routes/Subcategory');
+const productsRouter = require('./routes/Products');
+const offersRouter = require('./routes/Offers');
+const supermarketsRouter = require('./routes/Super_markets');
+const ReactionsRouter = require('./routes/Reactions');
+
 app.use('/auth', usersRouter);
+app.use('/categories', categoriesRouter);
+app.use('/subcategories', subcategoriesRouter);
+app.use('/products', productsRouter);
+app.use('/offers', offersRouter);
+app.use('/supermarkets', supermarketsRouter);
+app.use('/reactions', ReactionsRouter);
+
 
 db.sequelize.sync().then(() => {
     app.listen(3000, () => {
