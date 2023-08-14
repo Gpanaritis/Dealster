020b37eb41323a840940bd358151cf110d6ea995
Gpanaritis <jpanaritis2001@gmail.com>
2023-08-15 01:51:02 +0300

    Added auto logout when token expiration

--- client/src/services/auth-header.js
+++ client/src/services/auth-header.js
@@ -1,5 +1,24 @@
+import AuthService from './auth.service';
+
+const parseJwt = (token) => {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch (e) {
+    return null;
+  }
+};
+
 export default function authHeader() {
   const user = JSON.parse(localStorage.getItem('user'));
+
+  // check if the token has expired
+  if (user) {
+    const decodedJwt = parseJwt(user.accessToken);
+
+    if (decodedJwt.exp * 1000 < Date.now()) {
+      AuthService.logout();
+    }
+  }
 
   if (user && user.accessToken) {
     // return { Authorization: 'Bearer ' + user.accessToken }; // for Spring Boot back-end
